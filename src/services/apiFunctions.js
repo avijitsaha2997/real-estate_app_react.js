e03be5773047a2359b927b6e8371ff778f6f2615
avijitsaha2997 <jit.avi.saha2997@gmail.com>
2023-09-23 00:34:44 +0600

    remove variable apiUrl

--- src/services/apiFunctions.js
+++ src/services/apiFunctions.js
@@ -6,7 +6,7 @@ const apiUrl = import.meta.env.VITE_REACT_APP_API_URL;
 export const getApiData = (lang, url, query) => {
   return new Promise((resolve, reject) => {
     axios
-      .get(`${apiUrl}/${lang}/${url}/?${query}`)
+      .get(`http://52.77.121.171:3008/api/v1/${lang}/${url}/?${query}`)
       .then((response) => {
         resolve(response.data);
       })
@@ -18,7 +18,7 @@ export const getApiData = (lang, url, query) => {
 export const postApiData = async (data) => {
   return new Promise((resolve, reject) => {
     axios
-      .post(apiUrl + ".json", data)
+      .post("http://52.77.121.171:3008/api/v1" + ".json", data)
       .then((response) => {
         resolve(response.data);
       })
@@ -30,7 +30,9 @@ export const postApiData = async (data) => {
 export const putApiData = (url, taskId, data) => {
   return new Promise((resolve, reject) => {
     axios
-      .put(apiUrl + "/" + taskId + ".json", { data })
+      .put("http://52.77.121.171:3008/api/v1" + "/" + taskId + ".json", {
+        data,
+      })
       .then((response) => {
         resolve(response.data);
       })
